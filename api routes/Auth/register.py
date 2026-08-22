421213671ffc7eaaf14a5da62a6f60edf27ba666
Paulo Gabriel <gab.lyma7@gmail.com>
2026-08-22 00:10:40 -0300

    rota de registro

--- api routes/Auth/register.py
+++ api routes/Auth/register.py
@@ -1,24 +1,38 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr, ValidationError
+from pydantic import BaseModel, EmailStr, ValidationError, Field
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
+import psycopg2
+import datetime
+
+connection = psycopg2.connect(
+    dbname="content_trackerDB",
+    host="localhost",
+    port="5432",
+    user="postgres",
+    password="dekudeku7",
+)
+
+cursor = connection.cursor()
 
 pswdHasher = PasswordHasher()
 app = FastAPI()
 
 class Register(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8, max_length=64)
     name: str
 
 
 @app.post("/register")
 async def register(dados: Register):
     hashed = pswdHasher.hash(dados.password)
-    print(hashed)
+    sql = "INSERT INTO users (email, password, name, created_at) VALUES (%s, %s, %s, %s)"
+
     try:
-        pswdHasher.verify(hashed, dados.password)
-        print("ta engual")
+        cursor.execute(sql, (dados.email, hashed, dados.name, datetime.date.today()))
+        connection.commit()
+
     except VerifyMismatchError:
         print("verification error")
     return dados
