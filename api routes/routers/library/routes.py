a987a0e02592e08b01a1e6ba5c5c1b70f409ddc3
Paulo Gabriel <gab.lyma7@gmail.com>
2026-09-14 18:36:43 -0300

    conexao com api externa de filmes

--- api routes/routers/library/routes.py
+++ api routes/routers/library/routes.py
@@ -7,7 +7,8 @@ import jwt
 from sqlalchemy.testing.pickleable import User
 from pymongo import MongoClient
 import unicodedata
-
+import requests
+from pydantic import BaseModel
 # ----CONEXÕES----
 connection_string = os.getenv("CONNECTION_STRING")
 
@@ -28,6 +29,31 @@ cursor = connection.cursor()
 
 router = APIRouter(tags=["Libr"])
 
+class ContentRequest(BaseModel):
+    title: str
+    type: str
+
+TMDB_GENRES = {
+    28: "Ação",
+    12: "Aventura",
+    16: "Animação",
+    35: "Comédia",
+    80: "Crime",
+    99: "Documentário",
+    18: "Drama",
+    10751: "Família",
+    14: "Fantasia",
+    36: "História",
+    27: "Terror",
+    10402: "Música",
+    9648: "Mistério",
+    10749: "Romance",
+    878: "Ficção científica",
+    10770: "Cinema TV",
+    53: "Thriller",
+    10752: "Guerra",
+    37: "Faroeste"
+}
 # ----FUNÇÕES----
 def normalize_text(text: str) -> str:
     text = text.lower().strip()
@@ -39,81 +65,124 @@ def normalize_text(text: str) -> str:
     )
     return text
 
-def add_mongo():
+
+# def add_mongo():
+#     client = MongoClient(connection_string)
+#     db = client[os.getenv("DATABASE")]
+#     colecao = db[os.getenv("COLECAO")]
+#     dados = [
+#     {
+#         "title": "The Last Kingdom",
+#         "release_date": "2024-05-12",
+#         "summary": "Um RPG de ação em mundo aberto ambientado na era viking.",
+#         "image": "https://example.com",
+#         "genres": ["Action", "RPG", "Adventure"],
+#         "game_modes": ["Single-player", "Co-op"],
+#         "type": "Game"
+#     },
+#                         {
+#                             "title": "Space Velocity",
+#                             "release_date": "2025-11-20",
+#                             "summary": "Simulador de corrida espacial em alta velocidade com física realista.",
+#                             "image": "https://example.com",
+#                             "genres": ["Racing", "Simulation", "Sci-Fi"],
+#                             "game_modes": ["Single-player", "Multiplayer"],
+#                             "type": "Game"
+#                         },
+#     {
+#         "title": "Ecos do Amanhã",
+#         "release_date": "2023-08-15",
+#         "genres": ["Sci-Fi", "Drama", "Thriller"],
+#         "summary": "Um cientista descobre uma forma de receber mensagens do futuro, mas as consequências são catastróficas.",
+#         "image": "https://example.com",
+#         "type": "Movie"
+#     },
+#     {
+#         "title": "A Herança de Crimson",
+#         "release_date": "2026-02-05",
+#         "genres": ["Horror", "Mystery"],
+#         "summary": "Uma família herda uma mansão isolada e descobre segredos sombrios escondidos nas paredes.",
+#         "image": "https://example.com",
+#         "type": "Movie"
+#     },
+#     {
+#         "title": "O Império de Cinzas",
+#         "subtitle": "A Queda dos Três Reis",
+#         "authors": ["G. R. Martin", "J. R. R. Tolkien"],
+#         "release_date": "2021-03-30",
+#         "pages": 542,
+#         "image": "https://example.com",
+#         "type": "Book"
+#     },
+#     {
+#         "title": "Algoritmos do Pensamento",
+#         "subtitle": "Como a Inteligência Artificial Molda a Mente Humana",
+#         "authors": ["Ana Silva"],
+#         "release_date": "2025-09-01",
+#         "pages": 320,
+#         "image": "https://example.com",
+#         "type": "Book"
+#     }
+#     ]
+#     resultado = colecao.insert_many(dados)
+#     print(f"Documentos inseridos com sucesso! IDs: {resultado.inserted_ids}")
+#     client.close()
+#     return {"message": "Documentos inseridos com sucesso!"}
+
+
+
+def search_in_mongo(title, tipo):
+    normal_title = normalize_text(title)
     client = MongoClient(connection_string)
     db = client[os.getenv("DATABASE")]
     colecao = db[os.getenv("COLECAO")]
-    dados = [
-    {
-        "title": "The Last Kingdom",
-        "release_date": "2024-05-12",
-        "summary": "Um RPG de ação em mundo aberto ambientado na era viking.",
-        "image": "https://example.com",
-        "genres": ["Action", "RPG", "Adventure"],
-        "game_modes": ["Single-player", "Co-op"],
-        "type": "Game"
-    },
-                        {
-                            "title": "Space Velocity",
-                            "release_date": "2025-11-20",
-                            "summary": "Simulador de corrida espacial em alta velocidade com física realista.",
-                            "image": "https://example.com",
-                            "genres": ["Racing", "Simulation", "Sci-Fi"],
-                            "game_modes": ["Single-player", "Multiplayer"],
-                            "type": "Game"
-                        },
-    {
-        "title": "Ecos do Amanhã",
-        "release_date": "2023-08-15",
-        "genres": ["Sci-Fi", "Drama", "Thriller"],
-        "summary": "Um cientista descobre uma forma de receber mensagens do futuro, mas as consequências são catastróficas.",
-        "image": "https://example.com",
-        "type": "Movie"
-    },
-    {
-        "title": "A Herança de Crimson",
-        "release_date": "2026-02-05",
-        "genres": ["Horror", "Mystery"],
-        "summary": "Uma família herda uma mansão isolada e descobre segredos sombrios escondidos nas paredes.",
-        "image": "https://example.com",
-        "type": "Movie"
-    },
-    {
-        "title": "O Império de Cinzas",
-        "subtitle": "A Queda dos Três Reis",
-        "authors": ["G. R. Martin", "J. R. R. Tolkien"],
-        "release_date": "2021-03-30",
-        "pages": 542,
-        "image": "https://example.com",
-        "type": "Book"
-    },
-    {
-        "title": "Algoritmos do Pensamento",
-        "subtitle": "Como a Inteligência Artificial Molda a Mente Humana",
-        "authors": ["Ana Silva"],
-        "release_date": "2025-09-01",
-        "pages": 320,
-        "image": "https://example.com",
-        "type": "Book"
-    }
-    ]
-    resultado = colecao.insert_many(dados)
-    print(f"Documentos inseridos com sucesso! IDs: {resultado.inserted_ids}")
-    client.close()
-    return {"message": "Documentos inseridos com sucesso!"}
-
-
-
-def search_in_mongo(title):
-    # normal_title = normalize_text(title)
-    client = MongoClient(connection_string)
-    db = client[os.getenv("DATABASE")]
-    colecao = db[os.getenv("COLECAO")]
-
-    results = colecao.find({"title": {"$regex": title}})
+    filme = None
+    results = colecao.find({"title": {"$regex": title}, "type": tipo})
     for doc in results:
-        print(doc)
-    return results
+        filme = doc
+    return filme
+
+
+def search_movie_in_external_api(title):
+    tmdb_key = os.getenv("TMDB_KEY")
+    url = "https://api.themoviedb.org/3/search/movie"
+    main_title = normalize_text(title)
+    params = {"api_key": tmdb_key,
+              "query": main_title,
+              "language": "pt-BR"
+              }
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        results = response.json().get("results", [])
+        if results:
+            primeiro_filme = results[0]
+
+            genres = [
+                TMDB_GENRES[genre_id]
+                for genre_id in primeiro_filme.get("genre_ids", [])
+                if genre_id in TMDB_GENRES
+            ]
+
+            poster_path = primeiro_filme.get("poster_path")
+            if poster_path:
+                poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
+            else:
+                poster_url = None
+
+            return {
+                "external_id": primeiro_filme["id"],
+                "title": normalize_text(primeiro_filme["title"]),
+                "original_title": normalize_text(primeiro_filme["original_title"]),
+                "overview": primeiro_filme["overview"],
+                "date": primeiro_filme["release_date"],
+                "genres": genres,
+                "poster": poster_url,
+                "type": "movie"
+
+            }
+        else:
+            print("Nenhum filme encontrado.")
 
 def get_current_user(request: Request):
     access_token = request.cookies.get("access_token")
@@ -161,6 +230,7 @@ def get_current_user(request: Request):
 # ----ROTAS----
 @router.get("/library")
 async def get_library(user_id: str = Depends(get_current_user)):
+
     sql = "select * from userlibrary where user_id = %s"
 
     cursor.execute(sql, (user_id,))
@@ -168,11 +238,71 @@ async def get_library(user_id: str = Depends(get_current_user)):
     return cursor.fetchall()
 
 @router.post("/library")
-async def add_content_to_library(user: User = Depends(get_current_user)):
-    insert_sql = "insert into userlibrary values (%s, %s, %s, %s, %s, %s, %s)"
+async def add_content_to_library(title: ContentRequest, user = Depends(get_current_user)):
+    client = MongoClient(connection_string)
+    db = client[os.getenv("DATABASE")]
+    colecao = db[os.getenv("COLECAO")]
 
-@router.get("/teste")
-async def teste_mongo():
-    search_in_mongo("Ecos do Amanhã")
+
+    insert_sql = "insert into userlibrary (user_id, content_id, name, status, rating, created_at, external_api_id) values (%s, %s, %s, %s, %s, %s, %s)"
+    search_sql = "select * from userlibrary where user_id = %s and name = %s"
+    cursor.execute(search_sql, (int(user), title.title))
+    exist = cursor.fetchone()
+    if exist:
+        client.close()
+        return "filme ja existe no seu catálogo."
+
+# BUSCA DO MONGODB
+    id_externo = search_movie_in_external_api(title.title)["external_id"]
+
+    filme = colecao.find_one({
+        "external_id": id_externo
+    })
+    if not filme:
+        dados = search_movie_in_external_api(title.title)
+        if not dados:
+            client.close()
+            return "Filme não encontrado."
+
+
+        insert_data = colecao.insert_one(dados)
+        filme_name = dados["title"]
+        filme_id = dados["external_id"]
+
+        cursor.execute(insert_sql,
+                       (
+                           int(user),
+                           str(filme_id),
+                           filme_name,
+                           "Quero Assistir",
+                           None,
+                           datetime.date.today(),
+                           id_externo
+                       ))
+        connection.commit()
+
+    else:
+        print("bateu no else")
+        filme_id = filme["_id"]
+        filme_nome = filme["title"]
+        external_api_id = filme["external_id"]
+        print(external_api_id)
+        cursor.execute(insert_sql,
+                        (
+                        int(user),
+                        str(filme_id),
+                        filme_nome,
+                        "Quero Assistir",
+                        None,
+                        datetime.date.today(),
+                        external_api_id
+                        ))
+        connection.commit()
+        client.close()
+        return "Filme adicionado com sucesso."
+# @router.get("/teste")
+# async def teste_mongo():
+#
+#     return search_movie_in_external_api("de volta para o futuro")
 
 
